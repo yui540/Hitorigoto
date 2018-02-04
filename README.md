5648cc39b0b5cc0538bc8dbbae78677e8f5c7f13
yuki540 <tabletennis540@gmail.com>
2018-02-04 14:44:12 +0900

    初期環境の構築

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 yuki540.com version5.0.0のテーマは、「ヒトリゴト」です。
 
 ![sitename](./screenshots/sitename.png)
+
+## ローカルで確認
+
+#### インストール
+```bash
+$ npm install
+```
+
+#### ビルド
+```bash
+$ npm run build
+```
+
+#### 起動
+```bash
+$ npm run start
+```
---
+++ gulpfile.babel.js
@@ -0,0 +1,40 @@
+'use strict'
+
+import gulp from 'gulp'
+import gutil from 'gutil'
+import sass from 'gulp-sass'
+import sassGlob from 'gulp-sass-glob'
+import browserify from 'browserify'
+import source from 'vinyl-source-stream'
+import babelify from 'babelify'
+
+const SRC = './src'
+const DEST = './public'
+const SCRIPT_NAME = 'app.js'
+const BUNDLE_NAME = 'app.min.js'
+
+gulp.task('css', () => {
+  return gulp.src(`${ SRC }/scss/style.scss`)
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(gulp.dest(`${ DEST }/stylesheets`))
+})
+
+gulp.task('js', () => {
+  return browserify(`${ SRC }/scripts/${ SCRIPT_NAME }`, { debug: true })
+    .transform(babelify)
+    .bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error.'))
+    .pipe(source(`${ BUNDLE_NAME }`))
+    .pipe(gulp.dest(`${ DEST }/scripts`))
+})
+
+gulp.task('default', ['js', 'css'])
+
+gulp.task('watch', () => {
+  gulp.watch([
+    `${ SRC }/**/*.js`,
+    `${ SRC }/**/*.json`,
+    `${ SRC }/**/*.scss`
+  ], ['js', 'css'])
+})
