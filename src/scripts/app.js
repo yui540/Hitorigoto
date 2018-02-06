b582c450b95fee7cc994f500c85e65caa13f7465
yuki540 <tabletennis540@gmail.com>
2018-02-06 14:44:29 +0900

    update: 画像のプリロード周りの処理を実装

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,14 @@
-console.log('Hello friend.')
+import * as util from './lib/util'
+import preload_json from './config/preload'
+
+console.log('###############################################')
+console.log('                 ヒトリゴト')
+console.log('         「推しキャラは、コロコロ変わる。」')
+console.log('###############################################')
+console.log('Designed by yuki540.')
+
+util.preload(preload_json, () => {
+  util.finLoadView()  
+}, data => {
+  util.updateProgressBar(data.per)
+})
---
+++ src/scripts/config/preload.js
@@ -0,0 +1,3 @@
+export default [
+  './images/profile/yuki540.png'
+]
