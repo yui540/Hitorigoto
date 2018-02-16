fd451c57dc0165cf986d16a5c0a1cb6060e89cfe
yuki540 <tabletennis540@gmail.com>
2018-02-16 15:33:16 +0900

    fix: リンクの指定ミスを修正

--- src/scripts/components/history-box.jsx
+++ src/scripts/components/history-box.jsx
@@ -17,7 +17,7 @@ export default class HistoryBox extends React.Component {
             </div>
             <h3 className="title">{ history.title }</h3>
             <img src={ history.thumb } alt={ history.title } />
-            <a href={ history } target="_blank">
+            <a href={ history.link } target="_blank">
               v{ history.version }を見る
             </a>
           </section>
