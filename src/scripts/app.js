990fb7dcd4254bc7baa70ab6ad07159e1ce6f641
yuki540 <tabletennis540@gmail.com>
2018-02-16 16:25:14 +0900

    add: product-boxコンポーネントを追加

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,9 +3,11 @@ import { render } from 'react-dom'
 import * as util from './lib/util'
 import preload_json from './config/preload'
 import history_json from './config/history'
+import product_json from './config/product'
 
 // components
 import HistoryBox from './components/history-box.jsx'
+import ProductBox from './components/product-box.jsx'
 
 console.log('###############################################')
 console.log('                 ヒトリゴト')
@@ -16,6 +18,11 @@ console.log('Designed by yuki540.')
 render(
   <HistoryBox json={ history_json } />,
   document.getElementById('history-box')
+)
+
+render(
+  <ProductBox json={ product_json } />,
+  document.getElementById('product-box')
 )
 
 // bind event
---
+++ src/scripts/config/product.js
@@ -0,0 +1,94 @@
+export default {
+  "app": [
+    {
+      "name": "satella.io",
+      "link": "https://github.com/yuki540net/satella.io",
+      "thumb": "./images/product/satella.io.png",
+      "description": "イラストに「命」を吹き込むソフトウェア",
+      "date": "2017/02/05"
+    },
+    {
+      "name": "Shuvi",
+      "link": "https://github.com/yuki540net/Shuvi",
+      "thumb": "./images/product/shuvi.png",
+      "description": "フレームレスなYouTubeプレイヤー",
+      "date": "2017/05/13"
+    },
+    {
+      "name": "若葉",
+      "link": "https://github.com/yuki540net/wakaba",
+      "thumb": "./images/product/wakaba.png",
+      "description": "PC画面上にコメントが流せるデスクトップアプリ",
+      "date": "2017/03/19"
+    },
+    {
+      "name": "兎亜",
+      "link": "https://github.com/yuki540net/toa",
+      "thumb": "./images/product/toa.png",
+      "description": "シンプルで可愛いミュージックプレイヤー",
+      "date": "2017/04/08"
+    },
+    {
+      "name": "音卵",
+      "link": "https://github.com/yuki540net/ontama",
+      "thumb": "./images/product/ontama.png",
+      "description": "声優ラジオ音泉をダウンロードするAndroidアプリ",
+      "date": "2017/04/15"
+    },
+    {
+      "name": "音泉ダウンローダー",
+      "link": "https://github.com/yuki540net/onsen-downloader",
+      "thumb": "./images/product/onsen-downloader.png",
+      "description": "声優ラジオ音泉をダウンロードするChrome拡張",
+      "date": "2017/02/25"
+    }
+  ],
+  "lib": [
+    {
+      "name": "shuvi-lib",
+      "link": "https://github.com/yuki540net/shuvi-lib",
+      "thumb": "./images/product/shuvi-lib.jpg",
+      "description": "YouTube IFrame Player APIをゆるふわラッピングしたライブラリ",
+      "date": "2017/05/14"
+    },
+    {
+      "name": "onsen-node",
+      "link": "https://github.com/yuki540net/onsen-node",
+      "thumb": "./images/product/onsen-node.png",
+      "description": "声優ラジオ音泉APIライブラリ for Node",
+      "date": "2017/02/25"
+    },
+    {
+      "name": "nicoJS",
+      "link": "https://github.com/yuki540net/nicoJS",
+      "thumb": "./images/product/nicoJS.png",
+      "description": "ニコニコ風コメントを実装するライブラリ",
+      "date": "2016/09/25"
+    }
+  ],
+  "design": [
+    {
+      "name": "三玖",
+      "link": "http://miku.yuki540.com",
+      "thumb": "./images/product/miku.png",
+      "description": "『中野三玖は、可愛い』",
+      "date": "2018/01/11"
+    },
+    {
+      "name": "俺ガイル非公式サイト",
+      "link": "http://oregairu.yuki540.com",
+      "thumb": "./images/product/oregairu.png",
+      "description": "『俺は、本物が欲しい。』",
+      "date": "2016/03/20"
+    }
+  ],
+  "movie": [
+    {
+      "name": "COLORFUL_STEP",
+      "link": "http://www.nicovideo.jp/watch/sm26374280",
+      "thumb": "./images/product/colorful_step.png",
+      "description": "待ち望んだ全ては------そこにあるの。",
+      "date": "2015/05/30"
+    }
+  ]
+}
